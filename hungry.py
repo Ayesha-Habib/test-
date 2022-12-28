0d851502de9b9e58ba81085aedc2e122b4697381
Ayesha <ayeshahabib654@gmail.com>
2022-12-28 12:20:46 +0500

    added burger+fries

--- hungry.py
+++ hungry.py
@@ -1,7 +1,8 @@
 hungry= input("are you hungry")
 if hungry=="yes":
     print("eat samosas")
-    print("drink coke")
+    print("eat burger")
+    print("eat fries")
 
 else:
     print("do your homework")
