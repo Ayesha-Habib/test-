7b8cafb65aa1e70e433da8c2fa8a97914dc22287
Ayesha <ayeshahabib654@gmail.com>
2022-12-28 12:32:32 +0500

    added biryani

--- hungry.py
+++ hungry.py
@@ -3,6 +3,7 @@ if hungry=="yes":
     print("eat samosas")
     print("eat burger")
     print("eat fries")
+    print("eat biryani")
 else:
     thirsty=input("Are you thirsty?:")
     if thirsty=="yes":
